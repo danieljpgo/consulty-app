a41f8f24c28840f1dabcf673ea94d01811ab4426
Daniel Jorge <djpgonzaga@gmail.com>
2020-08-31 23:56:32 -0300

    fix: text size component

--- src/common/components/Text/index.ts
+++ src/common/components/Text/index.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components/native';
 
 interface TextProps {
   fontFamily: 'Poppins' | 'Archivo';
+  size: 'large' | 'medium' | 'small';
 }
 
 const fontFamily = {
@@ -13,10 +14,22 @@ const fontFamily = {
   `,
 };
 
+const size = {
+  large: css`
+    font-size: ${(props) => props.theme.fontSize.large}px;
+  `,
+  medium: css`
+    font-size: ${(props) => props.theme.fontSize.medium}px;
+  `,
+  small: css`
+    font-size: ${(props) => props.theme.fontSize.small}px;
+  `,
+};
+
 const Text = styled.Text<TextProps>`
   color: ${(props) => props.theme.colors.text.light};
-  font-size: ${(props) => props.theme.fontSize.medium}px;
   ${(props) => fontFamily[props.fontFamily]};
+  ${(props) => size[props.size]};
 `;
 
 export default Text;
